f1d666cb2dc1ff7c4a7ce998d284671e7696a7ca
onur  coskun <onur.coskun@wunder.is>
2023-09-03 22:42:22 +0300

    chore: logo component update

--- src/components/Logo/logo.tsx
+++ src/components/Logo/logo.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './logo.scss'
 import LogoImage from '../../assets/svg/logo.svg'
-function Logo() {
+
+const Logo = React.memo(function Logo() {
   return (
     <div className='Logo'>
       <img src={LogoImage} alt='AppointEaseLogo' />
     </div>
   )
-}
+})
 
 export default Logo
