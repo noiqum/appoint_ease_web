6b011c0363e2a4fdca1de070765ea799e9781313
onur  coskun <onur.coskun@wunder.is>
2023-09-05 21:31:06 +0300

    feat: tailwindCss config

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,5 +2,9 @@ import React from 'react'
 import './LoginPage.scss'
 
 export default function LoginPage() {
-  return <div data-testid='LoginPage' className='LoginPage'></div>
+  return (
+    <div data-testid='LoginPage' className='LoginPage'>
+      <p>Loginpage</p>
+    </div>
+  )
 }
