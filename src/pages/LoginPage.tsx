5dfd9563d8c082ee9051699cda567ff0a6413886
onur  coskun <onur.coskun@wunder.is>
2023-09-05 22:19:39 +0300

    feat: shadcn ui config

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './LoginPage.scss'
+import { Button } from '../components/Button/Button'
 
 export default function LoginPage() {
   return (
     <div data-testid='LoginPage' className='LoginPage'>
       <p>Loginpage</p>
+      <Button variant='outline'>Button</Button>
     </div>
   )
 }
