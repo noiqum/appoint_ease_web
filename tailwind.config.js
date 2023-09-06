5dbe4ef87b949f755536ef6dfaa46a0673227217
onur  coskun <onur.coskun@wunder.is>
2023-09-06 21:55:13 +0300

    feat: tailwind storybook config

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        lightgrey: '#ced4da',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
