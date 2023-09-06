acb849517c579d4e00242c341c918cf004c18b85
onur  coskun <onur.coskun@wunder.is>
2023-09-06 21:58:43 +0300

    fix: button label prop

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -38,10 +38,12 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, label, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button'
     return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+        {label}
+      </Comp>
     )
   },
 )
