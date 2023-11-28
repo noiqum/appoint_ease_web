82b933db558c02ffa5f77ecbd924bf90aa62c5b9
noiqum <onurcoskun99@gmail.com>
2023-11-28 18:31:30 +0300

    feat: logo component test

--- src/components/Logo/logo.test.tsx
+++ src/components/Logo/logo.test.tsx
@@ -1,9 +1,30 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import Logo from './logo'
 
+const mockUseNavigate = jest.fn()
+
+jest.mock('react-router-dom', () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const originalModule = jest.requireActual('react-router-dom') as any
+
+  return {
+    ...originalModule,
+    useNavigate: () => mockUseNavigate,
+  }
+})
 describe('Logo', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Logo />)
     expect(baseElement).toBeTruthy()
+  })
+  it('should redirect homepage with link prop true', () => {
+    const { baseElement } = render(<Logo link={true} />)
+    fireEvent.click(baseElement.querySelector('.Logo.link') as HTMLElement)
+    expect(mockUseNavigate).toHaveBeenCalledWith('/')
+  })
+  it('should not redirect homepage with link prop false', () => {
+    const { baseElement } = render(<Logo link={false} />)
+    fireEvent.click(baseElement.querySelector('.Logo') as HTMLElement)
+    expect(mockUseNavigate).not.toHaveBeenCalled()
   })
 })
