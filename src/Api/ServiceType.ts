8d10d4880a1ce3060b0da3931f81775f6321b212
noiqum <onurcoskun99@gmail.com>
2023-12-14 18:36:18 +0300

    feat: register service request type

--- src/Api/ServiceType.ts
+++ src/Api/ServiceType.ts
@@ -2,6 +2,11 @@ export type TLoginRequest = {
   email: string
   password: string
 }
+export type TRegisterRequest = {
+  name: string
+  email: string
+  password: string
+}
 
 export type TloginResponse = {
   _id: string
