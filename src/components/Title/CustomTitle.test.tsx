1054e7a511e84ecfa69a5805991675cfcdb32e39
noiqum <onurcoskun99@gmail.com>
2023-12-20 22:02:49 +0300

    feat: CustomTitle test case for render proper heading level

--- src/components/Title/CustomTitle.test.tsx
+++ src/components/Title/CustomTitle.test.tsx
@@ -3,11 +3,21 @@ import CustomTitle from './CustomTitle'
 import React from 'react'
 
 describe('Custom Title', () => {
-  it('Render level and text content prop properly', () => {
+  it('Render text content prop properly', () => {
     const TextElement = (text: string) => {
       return <p>{text}</p>
     }
     const titleElement = TextElement('test title')
-    render(<CustomTitle level={1} nestedElement={titleElement}></CustomTitle>)
+    const { getByText } = render(<CustomTitle level={1} nestedElement={titleElement}></CustomTitle>)
+    expect(getByText('test title')).toBeInTheDocument()
+  })
+  it('Render nested child level of h tag  properly', () => {
+    const TextElement = (text: string) => {
+      return <p>{text}</p>
+    }
+    const titleElement = TextElement('test title')
+    const { getByRole } = render(<CustomTitle level={1} nestedElement={titleElement}></CustomTitle>)
+    const CustomTitleRendered = getByRole('heading', { level: 1 })
+    expect(CustomTitleRendered).toBeInTheDocument()
   })
 })
