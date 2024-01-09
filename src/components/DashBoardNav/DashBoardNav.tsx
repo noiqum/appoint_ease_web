034305c9325f9187f3b8e1e874904e8da1bbe70a
noiqum <onurcoskun99@gmail.com>
2024-01-09 23:25:52 +0300

    feat: dashboard page buttons

--- src/components/DashBoardNav/DashBoardNav.tsx
+++ src/components/DashBoardNav/DashBoardNav.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import './DashBoardNav.scss'
+import { useAppSelector } from '../../store/hooks'
+import { RootState } from '../../store/index'
+import { Button } from '../Button/Button'
+import ExternalLinkIcon from '../../assets/svg/external-link.svg'
+import ShareLinkIcon from '../../assets/svg/share-link.svg'
+import PlusIcon from '../../assets/svg/plus.svg'
 
 export const DashBoardNav = () => {
-  return <div className='DashBoardNav'>DashBoardNav</div>
+  const user = useAppSelector((state: RootState) => state.auth.user)
+  return (
+    <div className='DashBoardNav'>
+      <div className='DashBoardNav__title'>
+        <span>
+          Meet with <span className='capitalize'>{user?.name}</span>
+        </span>
+      </div>
+      <div className='DashBoardNav__buttons'>
+        <Button icon={ExternalLinkIcon} variant='outline' label='View Live'></Button>
+        <Button icon={ShareLinkIcon} variant='outline' label='Share'></Button>
+        <Button icon={PlusIcon} variant='green' label='Create New'></Button>
+      </div>
+    </div>
+  )
 }
