337481b093407b750ccb7461ee56ab6a1cf75fe7
noiqum <onurcoskun99@gmail.com>
2024-01-15 22:09:02 +0300

    style: appointment modal style

--- src/components/AppointmentModal/AppointmentModal.tsx
+++ src/components/AppointmentModal/AppointmentModal.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import X from '../../assets/svg/x.svg'
 import NewAppointmentVisual from '../../assets/svg/new-one.svg'
-import { Form, FormControl, FormField, FormItem, FormLabel } from '../Form/Form'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../Form/Form'
 import { Button } from '../Button/Button'
 import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Input } from '../Input/Input'
+import './AppointmentModal.scss'
 
 const formSchema = z.object({
   name: z
     .string()
-    .min(5, 'Email must be at least 5 characters')
-    .max(50, 'Email cannot exceed 50 characters')
-    .nonempty('Email is required'),
+    .min(5, 'Appointment name must be at least 5 characters')
+    .max(50, 'Appointment name cannot exceed 50 characters')
+    .nonempty('Appointment name is required'),
 })
 export const AppointmentModal = () => {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -28,38 +29,46 @@ export const AppointmentModal = () => {
   }
   return (
     <div className='AppointmentModal'>
-      <span className='AppointModal__closeIcon'>
-        <img src={X} alt='close_modal_icon' />
-      </span>
-      <div className='AppointmentModal__image'>
-        <img src={NewAppointmentVisual} alt='' />
-      </div>
-      <div className='AppointmentModal__text'>
-        <h2>New Appointment</h2>
-        <p>
-          Meeting types are the services that people want to schedule you for. They will appear on
-          your scheduling page, and have a direct link you can share.
-        </p>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name='name'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder='Name' {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <Button variant='green' label='Create New Appointment' type='submit'>
-              Submit
-            </Button>
-          </form>
-        </Form>
-        <label htmlFor='appointment-name'></label>
+      <div
+        onClick={(e) => {
+          e.stopPropagation()
+        }}
+        className='AppointmentModal__box'
+      >
+        <span className='AppointmentModal__closeIcon'>
+          <img src={X} alt='close_modal_icon' />
+        </span>
+        <div className='AppointmentModal__image'>
+          <img src={NewAppointmentVisual} alt='' />
+        </div>
+        <div className='AppointmentModal__text'>
+          <h2>New Appointment</h2>
+          <p>
+            Meeting types are the services that people want to schedule you for. They will appear on
+            your scheduling page, and have a direct link you can share.
+          </p>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Name' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button variant='green' label='Create New Appointment' type='submit'>
+                Submit
+              </Button>
+            </form>
+          </Form>
+          <label htmlFor='appointment-name'></label>
+        </div>
       </div>
     </div>
   )
