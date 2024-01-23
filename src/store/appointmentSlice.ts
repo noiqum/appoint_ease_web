759c9572a3b75426e253f7ee8f9d9bc510dc686e
noiqum <onurcoskun99@gmail.com>
2024-01-23 16:19:47 +0300

    feat: appointment store

---
+++ src/store/appointmentSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { TAppointmentResponse } from '../Api/ServiceType'
+
+interface TinitialAppointmentState {
+  list: TAppointmentResponse[]
+  selected: TAppointmentResponse | null
+}
+
+const initialState: TinitialAppointmentState = {
+  list: [],
+  selected: null,
+}
+
+export const appointmentSlice = createSlice({
+  initialState,
+  name: 'AppointmentStore',
+  reducers: {
+    setAppointmentList: (state, action) => {
+      state.list = action.payload
+    },
+    setSelectedAppointment: (state, action) => {
+      state.selected = action.payload
+    },
+  },
+})
+export const { setAppointmentList, setSelectedAppointment } = appointmentSlice.actions
+export default appointmentSlice
