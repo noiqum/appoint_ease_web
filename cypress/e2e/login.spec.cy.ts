6f5785e1d094dce9179523701e12240008720f4e
noiqum <onurcoskun99@gmail.com>
2024-02-15 22:49:41 +0300

    test: login flow error handling desktop

--- cypress/e2e/login.spec.cy.ts
+++ cypress/e2e/login.spec.cy.ts
@@ -24,4 +24,17 @@ describe('Login', () => {
     cy.get("button[type='submit']").click()
     cy.url({ timeout: 10000 }).should('eq', 'http://localhost:3000/dashboard')
   })
+  it('should not login with invalid credentials in desktop and get error', () => {
+    cy.viewport(1440, 900)
+    cy.visit('/')
+    cy.get('.navigation__menu').within(() => {
+      cy.get('a[href="/login"]').click()
+    })
+
+    cy.get("input[name='email']").type('mike')
+    cy.get("input[name='password']").type('test1234')
+    cy.get("button[type='submit']").click()
+    cy.get('.text-destructive').contains('Please enter a valid email address')
+    cy.url().should('eq', 'http://localhost:3000/login')
+  })
 })
