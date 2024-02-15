b154d1953283ead1e6d16c3b1e67e697a86fd4e4
noiqum <onurcoskun99@gmail.com>
2024-02-15 22:22:19 +0300

    test: mobile login flow

--- cypress/e2e/login.spec.cy.ts
+++ cypress/e2e/login.spec.cy.ts
@@ -1,12 +1,15 @@
 describe('Login', () => {
-  it('should login with valid credentials', () => {
-    cy.visit('/login')
+  it('should login with valid credentials and redirect to dashboard in mobile', () => {
+    cy.viewport(375, 812)
+    cy.visit('/')
+    cy.get('.navigation__hamburger').click()
+    cy.get('.navigation__aside').within(() => {
+      cy.get("a[href='/login']").click()
+    })
 
-    cy.get('input[name="email"]').type('mike@gmail.com')
-    cy.get('input[name="password"]').type('test1234')
-
-    cy.get('button[type="submit"]').click()
-
-    cy.url().should('eq', 'http://localhost:3000/dashboard')
+    cy.get("input[name='email']").type('mike@gmail.com')
+    cy.get("input[name='password']").type('test1234')
+    cy.get("button[type='submit']").click()
+    cy.url({ timeout: 10000 }).should('eq', 'http://localhost:3000/dashboard')
   })
 })
