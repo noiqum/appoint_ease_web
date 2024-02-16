b6fdec91adb4c6a57f3ab0762b74a5b7ddbf1147
noiqum <onurcoskun99@gmail.com>
2024-02-16 22:00:19 +0300

    chore: update modal base

--- src/components/UpdateModal/UpdateModal.tsx
+++ src/components/UpdateModal/UpdateModal.tsx
@@ -4,6 +4,7 @@ import { Button } from '../Button/Button'
 import { useAppDispatch, useAppSelector } from '../../store/hooks'
 import { RootState } from '../../store/index'
 import modalSlice from '../../store/modalSlice'
+import * as Tabs from '@radix-ui/react-tabs'
 export const UpdateModal = () => {
   const dispatch = useAppDispatch()
   const selectedAppointment = useAppSelector((state: RootState) => state.appointment.selected)
@@ -32,8 +33,30 @@ export const UpdateModal = () => {
             />
           </div>
         </div>
-        <div className='UpdateModal__nav'></div>
-        <div className='UpdateModal__footer'></div>
+        <div className='UpdateModal__main'>
+          <Tabs.Root defaultValue='general' className='UpdateModal__main__root'>
+            <Tabs.List className='TabsList'>
+              <Tabs.Trigger className='UpdateModal__main__tab' value='general'>
+                General
+              </Tabs.Trigger>
+              <Tabs.Trigger className='UpdateModal__main__tab' value='availability'>
+                Availability
+              </Tabs.Trigger>
+              <Tabs.Trigger className='UpdateModal__main__tab' value='notes'>
+                Notes
+              </Tabs.Trigger>
+            </Tabs.List>
+            <Tabs.Content className='UpdateModal__main__content' value='general'>
+              General
+            </Tabs.Content>
+            <Tabs.Content className='UpdateModal__main__content' value='availability'>
+              Availability
+            </Tabs.Content>
+            <Tabs.Content className='UpdateModal__main__content' value='notes'>
+              Notes
+            </Tabs.Content>
+          </Tabs.Root>
+        </div>
       </div>
     </div>
   )
