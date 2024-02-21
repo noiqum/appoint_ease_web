b72b54e294a3bad360368c1e3e102fbdd83f6d33
noiqum <onurcoskun99@gmail.com>
2024-02-21 23:34:38 +0300

    feat: footer component base

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from '../Logo/logo'
+
+export const Footer = () => {
+  return (
+    <footer className='Footer'>
+      <div className='Footer__content'>
+        <div className='Footer__content__brand'>
+          <Logo link={true}></Logo> <span>AppointEase</span>
+        </div>
+        <div className='Footer__content__text'>
+          The simple and affordable way to book meetings online
+        </div>
+      </div>
+      <div className='Footer__links'></div>
+    </footer>
+  )
+}
