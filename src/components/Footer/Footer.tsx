a56e048b15b84118fd37f77adad1eef8d79b834b
noiqum <onurcoskun99@gmail.com>
2024-02-28 22:42:12 +0300

    chore: footer social links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import Logo from '../Logo/logo'
+import facebookLogo from '../../assets/svg/facebook-logo-blue.svg'
+import twitterLogo from '../../assets/svg/twitter-logo-blue.svg'
+import './Footer.scss'
+import instagramLogo from '../../assets/svg/instagram-logo-blue.svg'
+import linkedinLogo from '../../assets/svg/linkedin-logo-blue.svg'
 
 export const Footer = () => {
   return (
@@ -11,7 +16,33 @@ export const Footer = () => {
         <div className='Footer__content__text'>
           The simple and affordable way to book meetings online
         </div>
-        <div className='Footer__content__social'></div>
+        <div
+          onClick={(e) => {
+            e.preventDefault()
+          }}
+          className='Footer__content__social'
+        >
+          <a href='#'>
+            <span>
+              <img src={facebookLogo} alt='facebook' />
+            </span>
+          </a>
+          <a href='#'>
+            <span>
+              <img src={twitterLogo} alt='twitter' />
+            </span>
+          </a>
+          <a href='#'>
+            <span>
+              <img src={instagramLogo} alt='instagram' />
+            </span>
+          </a>
+          <a href='#'>
+            <span>
+              <img src={linkedinLogo} alt='linkedin' />
+            </span>
+          </a>
+        </div>
       </div>
       <div className='Footer__links'></div>
     </footer>
